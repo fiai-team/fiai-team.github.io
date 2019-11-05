5e3664f2ab1abbc36ed7ceafa5e97b9398a22a0d
Arael David Espinosa Pérez <cl8dep@gmail.com>
2019-11-05 13:38:46 -0500

    Projects added

---
+++ precache-manifest.11e87e1d4ac4a76cb03981d0f7511e96.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbdae88fad5c68d8944d4356778aa31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6bcd122a49ad0c3289b",
+    "url": "/static/js/2.9e486fe2.chunk.js"
+  },
+  {
+    "revision": "a3201cda65ec271955c7",
+    "url": "/static/js/main.09f4103e.chunk.js"
+  },
+  {
+    "revision": "1636350151cf2de6e6c5",
+    "url": "/static/js/runtime~main.4f81a56b.js"
+  },
+  {
+    "revision": "afb4f665e0ad2d45ce972179a9acfb6f",
+    "url": "/static/media/background.afb4f665.jpg"
+  },
+  {
+    "revision": "4ad08251ddecfdd6e2a2c53e79b8d8de",
+    "url": "/static/media/default.4ad08251.png"
+  },
+  {
+    "revision": "a2206aaffab4c3194bad840d161d0a92",
+    "url": "/static/media/fiai_icon_blue.a2206aaf.png"
+  },
+  {
+    "revision": "7fafe60909252968c306233b5078cb97",
+    "url": "/static/media/fiai_logo.7fafe609.png"
+  },
+  {
+    "revision": "e9de7760eb6f042e7909b4443c1c530f",
+    "url": "/static/media/relme.e9de7760.png"
+  },
+  {
+    "revision": "c88985f4c71f39435eee3eb880a8b014",
+    "url": "/static/media/uciciencia.c88985f4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.862fc317176ce5a311d331ca97598b3d.js"
+  "/precache-manifest.11e87e1d4ac4a76cb03981d0f7511e96.js"
 );
 
 self.addEventListener('message', (event) => {
